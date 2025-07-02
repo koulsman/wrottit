629c1b44e0ef4817b6c81db06310a58f8514c823
Stavros Koulas <koulas@verusplus.com>
2025-07-02 03:28:37 +0300

    login

--- src/Header/Login.jsx
+++ src/Header/Login.jsx
@@ -21,8 +21,8 @@ export function Login() {
 
   async function handleLogin() {
     try {
-      const response = await axios.post(`${config.USERS_API}/users/login`, 
-         
+      const response = await axios.post(`${config.POSTS_API}/users/login`, 
+        
         {
         email,
         password
