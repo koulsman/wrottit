7fa0f38ce1103de8610f6ed3dbe0b98e86c13b72
Stavros Koulas <koulas@verusplus.com>
2025-07-06 20:57:27 +0300

    reverse

--- src/config.js
+++ src/config.js
@@ -2,15 +2,15 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const config = {
   USERS_API: isProduction
-    ? `${process.env.REACT_APP_API_BASE_PROD}`
+    ? `${process.env.REACT_APP_API_BASE_PROD}/UserServer`
     : process.env.REACT_APP_USERS_API_DEV,
 
   POSTS_API: isProduction
-    ? `${process.env.REACT_APP_API_BASE_PROD}`
+    ? `${process.env.REACT_APP_API_BASE_PROD}/PostServer`
     : process.env.REACT_APP_POSTS_API_DEV,
 
   COMMUNITIES_API: isProduction
-    ? `${process.env.REACT_APP_API_BASE_PROD}`
+    ? `${process.env.REACT_APP_API_BASE_PROD}/CommunityServer`
     : process.env.REACT_APP_COMMUNITIES_API_DEV,
 };
 
